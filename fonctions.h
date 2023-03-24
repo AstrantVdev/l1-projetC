50f9a8be36434735db3615a34df95b1f0d3ad29d
AstrantV <AstrantV.creator@gmail.com>
2023-03-24 10:11:59 +0100

    fonction.h copié collé

--- fonctions.h
+++ fonctions.h
@@ -10,9 +10,95 @@ Point *create_point(int px, int py);
 void delete_point(Point * point);
 void print_point(Point * p);
 
+
 typedef struct line {
     Point *p1;
     Point *p2;
 }Line;
+
+Line *create_line(Point * p1, Point * p2);
+void delete_line(Line * line);
+void print_line(Line * line);
+
+
+typedef struct {
+    Point * p;
+    int length;
+}Square;
+
+Square *create_square(Point * point, int length);
+void delete_square(Square * square);
+void print_square(Square * square);
+
+
+typedef struct {
+    Point * p;
+    int width;
+    int length;
+}Rectangle;
+
+Rectangle *create_rectangle(Point * point, int width, int height);
+void delete_rectangle(Rectangle * rectangle);
+void print_rectangle(Rectangle * rectangle);
+
+
+typedef struct {
+    Point * point;
+    int radius;
+}Circle;
+
+Circle *create_circle(Point * center, int radius);
+void delete_circle(Circle * circle);
+void print_circle(Circle * circle);
+
+
+typedef struct polygon {
+    int n;
+    Point ** points; // tableau 1D dynamique de Point*
+}Polygon;
+
+Polygon *create_polygon(int n);
+void delete_polygon(Polygon * polygon);
+void print_polygon(Polygon * polygon);
+
+
+typedef enum { POINT, LINE, SQUARE, RECTANGLE, CIRCLE, POLYGON} SHAPE_TYPE;
+
+
+typedef struct shape {
+    int id; // identifiant unique de la forme
+    SHAPE_TYPE shape_type; // type de la forme pointé
+    void *ptrShape; // pointeur sur n'importe quelle forme
+}Shape;
+
+Shape *create_empty_shape(SHAPE_TYPE shape_type);
+Shape *create_point_shape(int px, int py);
+Shape *create_line_shape(int px1, int py1, int px2, int py2);
+Shape *create_square_shape(int px, int py, int length);
+Shape *create_rectangle_shape(int px, int py, int width, int height);
+Shape *create_circle_shape(int px, int py, int radus);
+Shape *create_polygon_shape(int lst[], int n);
+void delete_shape(Shape * shape);
+void print_shape(Shape * shape);
+
+Shape *create_empty_shape(SHAPE_TYPE shape_type) {
+    Shape *shp = (Shape *) malloc(sizeof(Shape));
+    shp->ptrShape = NULL;
+    shp->id = 1; // plus tard on appelera get_next_id();
+    shp->shape_type = shape_type;
+    return shp;
+}
+
+Shape *create_point_shape(int px, int py) {
+    Shape *shp = create_empty_shape(POINT);
+    Point *p = create_point(px, py);
+    shp->ptrShape = p;
+    return shp;
+}
+
+
+unsigned int get_next_id();
+
+
 
 #endif
---
+++ main.c
@@ -0,0 +1,8 @@
+#include <fonctions.h>
+unsigned int global_id = 0;
+
+int main (){
+
+
+    return 0;
+};
