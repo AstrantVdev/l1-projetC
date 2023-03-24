c87fb50820dadd41aae8b0a5eb9678923c8a394d
sacha-portal <sacha.portal@efrei.net>
2023-03-24 10:17:43 +0100

    Déplacement des définitions et fonctions relatif au shape dans le header shape.h

---
+++ shape.h
@@ -0,0 +1,46 @@
+//
+// Created by 33611 on 24/03/2023.
+//
+
+#ifndef L1_PROJETC_SHAPE_H
+#define L1_PROJETC_SHAPE_H
+
+typedef enum { POINT, LINE, SQUARE, RECTANGLE, CIRCLE, POLYGON} SHAPE_TYPE;
+
+
+typedef struct shape {
+    int id; // identifiant unique de la forme
+    SHAPE_TYPE shape_type; // type de la forme pointé
+    void *ptrShape; // pointeur sur n'importe quelle forme
+}Shape;
+
+Shape *create_empty_shape(SHAPE_TYPE shape_type);
+Shape *create_point_shape(int px, int py);
+Shape *create_line_shape(int px1, int py1, int px2, int py2);
+Shape *create_square_shape(int px, int py, int length);
+Shape *create_rectangle_shape(int px, int py, int width, int height);
+Shape *create_circle_shape(int px, int py, int radus);
+Shape *create_polygon_shape(int lst[], int n);
+void delete_shape(Shape * shape);
+void print_shape(Shape * shape);
+
+Shape *create_empty_shape(SHAPE_TYPE shape_type) {
+    Shape *shp = (Shape *) malloc(sizeof(Shape));
+    shp->ptrShape = NULL;
+    shp->id = 1; // plus tard on appelera get_next_id();
+    shp->shape_type = shape_type;
+    return shp;
+}
+
+Shape *create_point_shape(int px, int py) {
+    Shape *shp = create_empty_shape(POINT);
+    Point *p = create_point(px, py);
+    shp->ptrShape = p;
+    return shp;
+}
+
+
+unsigned int get_next_id();
+
+
+#endif //L1_PROJETC_SHAPE_H
