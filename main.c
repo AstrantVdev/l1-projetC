62828725b0eeb4b29bdfa15e92361e1b903b3d04
AstrantV <AstrantV.creator@gmail.com>
2023-04-05 15:43:09 +0200

    Update main.c

--- main.c
+++ main.c
@@ -1,14 +1,13 @@
 #include <stdio.h>
-#include <fonctions.h>
-#include <stdio.h>
+#include "fonctions.h"
 unsigned int global_id = 0;
 
 int main (){
-    printf("Zeqrstdyfugihguyftdrseqzazeqrstdyfug");
-    for(int i = 0; i < 25; i++){
-        printf("%d", i);
-        for(int j = 0; j < 80; j++) printf(".");
-        printf("\n");
+    int run = 1;
+
+    while(run){
+        printf("1");
     }
+
     return 0;
 };
