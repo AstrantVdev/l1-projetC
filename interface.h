0cf7d636a44983a32f4e9116183806730be857f5
Sacha Portal <sacha.portal@efrei.net>
2023-04-20 14:49:17 +0200

    Ajout de protoypes

--- interface.h
+++ interface.h
@@ -13,7 +13,11 @@ void print_list_shape(Shape* list_shape, int len);
 void print_dessin();
 void print_add_shape();
 
-int* add_point();
+int* add_points(int nb_points);
+void add_radius(int* radius);
+void add_size_rectangle(int* width, int* height);
+void add_length_square(int* length);
+void add_nb_points(int* nb_points);
 
 Shape *print_add_point();
 Shape *print_add_line();
